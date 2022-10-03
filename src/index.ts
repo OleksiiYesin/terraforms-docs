247dc72dc23173b2bc953038d8811b0961165390
Oleksii Yesin <leha.esin.95@gmail.com>
2022-10-03 13:26:09 +0300

    check: terraform-docs version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { getInput } from "@actions/core";
+import { execSync } from "child_process";
 
-const inputName = getInput("name");
+const workingDir = getInput("working_dir");
+const outputFormat = getInput("output_format");
+const outputMode = getInput("output_mode")
+const outputFile = getInput("output_file")
 
-greet(inputName);
 
-function greet(name: string) {
-  console.log(`'Hello ${name}!`);
+function checkReadme(workingDir: string, outputFormat: string, outputMode: string, outputFile: string) {
+  execSync(`terraform-docs ${outputFormat} --output-file ${outputFile} --output-mode ${outputMode} ${workingDir} --output-check`)
 }
+
+function run() {
+  checkReadme(workingDir,outputFormat,outputMode,outputFile)
+}
+
+run()

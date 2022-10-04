8e3e17e9b66e4aa0494cb406420389b8604d12fd
Oleksii Yesin <leha.esin.95@gmail.com>
2022-10-04 12:22:30 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@ import { getInput } from "@actions/core";
 import { getStdOutput } from './res/utils';
 import simpleGit from 'simple-git';
 
-const workingDir = getInput("tf_working_dir");
-const outputFormat = getInput("tf_output_format");
-const outputMode = getInput("tf_output_mode")
-const outputFile = getInput("tf_output_file")
-
 const git = simpleGit();
+
+const config = git.getConfig('*')
 
 const fetch = git.fetch(['--depth=1', 'origin', '+refs/tags/*:refs/tags/*'])
 const status = git.status(['--porcelain'])
 
 function run() {
+  console.log(config);
   console.log(fetch);
   console.log(status);
 }

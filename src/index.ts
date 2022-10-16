4aa93f2c7df47c6bc2e9cb224da82b405f37267a
OleksiiYesin <86779533+OleksiiYesin@users.noreply.github.com>
2022-10-16 13:43:19 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-import { getInput } from "@actions/core";
-import { getStdOutput } from "./res/utils";
+import { execSync } from 'child_process';
+import * as Diff from 'diff';
+require('colors');
 
-// const failOnDiff = getInput("tf_fail_on_diff");
-const failOnDiff = 'true'
+const file1: string = './action/examples/README.md'
+const file2: string = './action/examples/README_NEW.md'
 
-async function run() {
-  await gitStatus()
+
+const awk1 = execSync(`awk '/<!-- END_TF_DOCS -->/{found=0} {if(found) print} /<!-- BEGIN_TF_DOCS -->/{found=1}' ${file1}`).toString()
+const awk2 = execSync(`awk '/<!-- END_TF_DOCS -->/{found=0} {if(found) print} /<!-- BEGIN_TF_DOCS -->/{found=1}' ${file2}`).toString()
+
+if(awk1 === awk2) {
+  console.log(`======\nREADME.md is up to date!!\n======\n`);
+} else {
+  diff(awk1, awk2)
 }
 
-async function gitStatus() {
-  const num = async () => {return getStdOutput('git', [ 'status', '--porcelain', '|', 'grep', '/\*.tf', '|', 'grep', '-c', '-E', '([MA]\W).+' ])}
-  console.log(num);
-  
-}
 
-run()
+async function diff(file1: string, file2: string) {
+  const diff = Diff.diffLines(file1, file2);
+
+  diff.forEach((part) => {
+    let color: any = part.added ? 'green' : part.removed ? 'red' : 'grey' ;
+    process.stdout.write(part.value[color])
+  });
+}
